b9064dc25b3bf04967bcf6d4c035e696ae4e8e09
zzc <zzcqiyue@gmail.com>
2026-07-18 23:39:29 +0800

    chore: 发布 v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.example.videodownload"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         androidResources {
             localeFilters += listOf("zh", "en")
